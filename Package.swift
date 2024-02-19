258fc8a5b028ab33522be0f77f35add963b6134b
Alexander Gorbunov <fedulvtubudul@gmail.com>
2024-02-19 17:23:30 +0900

    build: Bump pod version to 0.8.1

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 
-let version = "0.8.0"
-let mediasoupChecksum = "fa999ead73012cfa9385fbfcfe5a7ffc7987ec0fa9c7873d05fd0349ef38493b"
-let webrtcChecksum = "d6f5f751ca3aad5ba9e74896ba6e5a4869b3a50ac6def2a069357277d94ea99a"
+let version = "0.8.1"
+let mediasoupChecksum = "d791d60fce826b7efd041c33b8576f0ccefb87c2f31fb216431b7ac294503a0e"
+let webrtcChecksum = "6fd8501746d1de14cc2b2aa86d1ea4e081d8ad50ad190c99fb12fdbfdf3257bf"
 
 
 let package = Package(

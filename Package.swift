fd257f742b2f4f2775f110da70e0e799a345c478
Alexander Gorbunov <gorbunov.a@vlprojects.pro>
2025-08-28 17:53:49 +0900

    build: Upload version 0.9.0

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 
-let version = "0.8.0"
-let mediasoupChecksum = "fa999ead73012cfa9385fbfcfe5a7ffc7987ec0fa9c7873d05fd0349ef38493b"
+let version = "0.9.0"
+let mediasoupChecksum = "ce70d0d16499a9972c0b627589946a18859552a6de09b97bb507f898e50504a9"
 let webrtcChecksum = "d6f5f751ca3aad5ba9e74896ba6e5a4869b3a50ac6def2a069357277d94ea99a"
 
 

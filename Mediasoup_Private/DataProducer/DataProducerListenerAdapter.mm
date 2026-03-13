af5e944412aed1060dd7557c025ca1f42fad0314
Alexander Gorbunov <gorbunov.a@vlprojects.pro>
2026-03-13 20:12:29 +0900

    feat: Implement obj-c wrapper for DataProducer listener

---
+++ Mediasoup_Private/DataProducer/DataProducerListenerAdapter.mm
@@ -0,0 +1,26 @@
+#import "DataProducerListenerAdapter.hpp"
+#import "DataProducerListenerAdapterDelegate.h"
+
+
+DataProducerListenerAdapter::DataProducerListenerAdapter() {
+}
+
+DataProducerListenerAdapter::~DataProducerListenerAdapter() {
+	this->delegate = nil;
+}
+
+void DataProducerListenerAdapter::OnOpen(mediasoupclient::DataProducer* dataProducer) {
+	[this->delegate onOpen:this];
+}
+
+void DataProducerListenerAdapter::OnClose(mediasoupclient::DataProducer* dataProducer) {
+	[this->delegate onClose:this];
+}
+
+void DataProducerListenerAdapter::OnBufferedAmountChange(mediasoupclient::DataProducer* dataProducer, uint64_t sentDataSize) {
+	[this->delegate onBufferedAmountChange:this dataSize:sentDataSize];
+}
+
+void DataProducerListenerAdapter::OnTransportClose(mediasoupclient::DataProducer* producer) {
+	[this->delegate onTransportClose:this];
+}
